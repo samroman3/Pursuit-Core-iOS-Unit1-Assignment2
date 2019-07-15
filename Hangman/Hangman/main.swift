2d4f233835845acd90b0f3b17784398d49f870ad
Sam <samroman3@gmail.com>
2019-07-15 11:14:06 -0500

    Hangman/Hangman.xcodeproj/project.xcworkspace/xcuserdata/samroman.xcuserdatad/UserInterfaceState.xcuserstate

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -1,12 +1,161 @@
 //
 //  main.swift
-//  Hangman
+//  Hangman_Project
 //
-//  Created by Alex Paul on 10/15/18.
-//  Copyright © 2018 Pursuit. All rights reserved.
+//  Created by Sam Roman on 7/12/19.
+//  Copyright © 2019 Sam Roman. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+//to run a swift file in terminal ,
+//type "swift [name of swift file"
+
+//to create an input in terminal use "return readLine()"
+//for example:
+
+//func getInput() -> String?{
+//    print("get input", terminator: "")
+//    return readLine()
+//}
+//
+//print(getInput())
+
+
+//you can save the user input in a variable in the function using
+//let lineIn = readLine()
+
+
+//you can use a while loop to loop through gameplay but be wary of infinite loops
+
+
+
+//guess the word - you play against the computer
+
+//you have seven guesses - seven body parts
+//if guessed letter is not in word add a body part to hangman
+// if guessed letter IS in word add(concaphenate) it to empy word placeholder
+//
+
+
+//Hangman Game
+
+
+let allTheWords = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake", "baby", "back", "balance", "ball", "band", "base", "basin", "basket", "bath", "beautiful", "because", "before", "behaviour", "belief", "bell", "bent", "berry", "between", "bird", "birth", "bite", "bitter", "black", "blade", "blood", "blow", "blue", "board", "boat", "body", "boiling", "bone", "book", "boot", "bottle", "brain", "brake", "branch", "brass", "bread", "breath", "brick", "bridge", "bright", "broken", "brother", "brown", "brush", "bucket", "building", "bulb", "burn", "burst", "business", "butter", "button", "cake", "camera", "canvas", "card", "care", "carriage", "cart", "cause", "certain", "chain", "chalk", "chance", "change", "cheap", "cheese", "chemical", "chest", "chief", "chin", "church", "circle", "clean", "clear", "clock", "cloth", "cloud", "coal", "coat", "cold", "collar", "colour", "comb", "come", "comfort", "committee", "common", "company", "comparison", "competition", "complete", "complex", "condition", "connection", "conscious", "control", "cook", "copper", "copy", "cord", "cork", "cotton", "cough", "country", "cover", "crack", "credit", "crime", "cruel", "crush", "current", "curtain", "curve", "cushion", "damage", "danger", "dark", "daughter", "dead", "dear", "death", "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction", "detail", "development", "different", "digestion", "direction", "dirty", "discovery", "discussion", "disease", "disgust", "distance", "distribution", "division", "door", "doubt", "down", "drain", "drawer", "dress", "drink", "driving", "drop", "dust", "early", "earth", "east", "edge", "education", "effect", "elastic", "electric", "engine", "enough", "equal", "error", "even", "event", "ever", "every", "example", "exchange", "existence", "expansion", "experience", "expert", "face", "fact", "fall", "false", "family", "farm", "father", "fear", "feather", "feeble", "feeling", "female", "fertile", "fiction", "field", "fight", "finger", "fire", "first", "fish", "fixed", "flag", "flame", "flat", "flight", "floor", "flower", "fold", "food", "foolish", "foot", "force", "fork", "form", "forward", "fowl", "frame", "free", "frequent", "friend", "from", "front", "fruit", "full", "future", "garden", "general", "girl", "give", "glass", "glove", "goat", "gold", "good", "government", "grain", "grass", "great", "green", "grey", "grip", "group", "growth", "guide", "hair", "hammer", "hand", "hanging", "happy", "harbour", "hard", "harmony", "hate", "have", "head", "healthy", "hear", "hearing", "heart", "heat", "help", "high", "history", "hole", "hollow", "hook", "hope", "horn", "horse", "hospital", "hour", "house", "humour", "idea", "important", "impulse", "increase", "industry", "insect", "instrument", "insurance", "interest", "invention", "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge", "land", "language", "last", "late", "laugh", "lead", "leaf", "learning", "leather", "left", "letter", "level", "library", "lift", "light", "like", "limit", "line", "linen", "liquid", "list", "little", "living", "lock", "long", "look", "loose", "loss", "loud", "love", "machine", "make", "male", "manager", "mark", "market", "married", "mass", "match", "material", "meal", "measure", "meat", "medical", "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed", "money", "monkey", "month", "moon", "morning", "mother", "motion", "mountain", "mouth", "move", "much", "muscle", "music", "nail", "name", "narrow", "nation", "natural", "near", "necessary", "neck", "need", "needle", "nerve", "news", "night", "noise", "normal", "north", "nose", "note", "number", "observation", "offer", "office", "only", "open", "operation", "opinion", "opposite", "orange", "order", "organization", "ornament", "other", "oven", "over", "owner", "page", "pain", "paint", "paper", "parallel", "parcel", "part", "past", "paste", "payment", "peace", "pencil", "person", "physical", "picture", "pipe", "place", "plane", "plant", "plate", "play", "please", "pleasure", "plough", "pocket", "point", "poison", "polish", "political", "poor", "porter", "position", "possible", "potato", "powder", "power", "present", "price", "print", "prison", "private", "probable", "process", "produce", "profit", "property", "prose", "protest", "public", "pull", "pump", "punishment", "purpose", "push", "quality", "question", "quick", "quiet", "quite", "rail", "rain", "range", "rate", "reaction", "reading", "ready", "reason", "receipt", "record", "regret", "regular", "relation", "religion", "representative", "request", "respect", "responsible", "rest", "reward", "rhythm", "rice", "right", "ring", "river", "road", "roll", "roof", "room", "root", "rough", "round", "rule", "safe", "sail", "salt", "same", "sand", "scale", "school", "science", "scissors", "screw", "seat", "second", "secret", "secretary", "seed", "seem", "selection", "self", "send", "sense", "separate", "serious", "servant", "shade", "shake", "shame", "sharp", "sheep", "shelf", "ship", "shirt", "shock", "shoe", "short", "shut", "side", "sign", "silk", "silver", "simple", "sister", "size", "skin", "skirt", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke", "smooth", "snake", "sneeze", "snow", "soap", "society", "sock", "soft", "solid", "some", "song", "sort", "sound", "soup", "south", "space", "spade", "special", "sponge", "spoon", "spring", "square", "stage", "stamp", "star", "start", "statement", "station", "steam", "steel", "stem", "step", "stick", "sticky", "stiff", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight", "strange", "street", "stretch", "strong", "structure", "substance", "such", "sudden", "sugar", "suggestion", "summer", "support", "surprise", "sweet", "swim", "system", "table", "tail", "take", "talk", "tall", "taste", "teaching", "tendency", "test", "than", "that", "then", "theory", "there", "thick", "thin", "thing", "this", "thought", "thread", "throat", "through", "through", "thumb", "thunder", "ticket", "tight", "till", "time", "tired", "together", "tomorrow", "tongue", "tooth", "touch", "town", "trade", "train", "transport", "tray", "tree", "trick", "trouble", "trousers", "true", "turn", "twist", "umbrella", "under", "unit", "value", "verse", "very", "vessel", "view", "violent", "voice", "waiting", "walk", "wall", "warm", "wash", "waste", "watch", "water", "wave", "weather", "week", "weight", "well", "west", "wheel", "when", "where", "while", "whip", "whistle", "white", "wide", "will", "wind", "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work", "worm", "wound", "writing", "wrong", "year", "yellow", "yesterday", "young"]
+
+//to run a swift file in terminal ,
+//type "swift [name of swift file"
+
+//to create an input in terminal use "return readLine()"
+//for example:
+
+//func getInput() -> String?{
+//    print("get input", terminator: "")
+//    return readLine()
+//}
+//
+//print(getInput())
+
+
+//you can save the user input in a variable in the function using
+//let lineIn = readLine()
+
+
+//you can use a while loop to loop through gameplay but be wary of infinite loops
+
+
+
+//guess the word - you play against the computer
+
+//you have seven guesses - seven body parts
+//if guessed letter is not in word add a body part to hangman
+// if guessed letter IS in word add(concaphenate) it to empy word placeholder
+//
+
+
+//Hangman Game
+var numberOfGuesses: Int = 0
+
+var allGuesses = [Character]()
+
+var randomWord = allTheWords.randomElement()
+
+var guess: Character = " "
+
+var answer = randomWord
+
+var stillPlaying = true
+
+//Prints blank if user hasnt guessed correctly
+func printRightGuesses()-> String {
+    var rightGuesses = ""
+    for letter in answer! {
+        if (letter == " ") {
+            rightGuesses += " "
+        } else {
+            var letterGuessed = false
+            for guess in allGuesses {
+                if (letter == guess) {
+                    letterGuessed = true
+                }
+            }
+            if letterGuessed {
+                rightGuesses += String(letter)
+            } else {
+                rightGuesses += "_"
+            }
+        }
+    }
+    return rightGuesses
+}
+
+//prints guessed letters
+func printGuessedLetters(){
+    if allGuesses.isEmpty {
+        print("nothing")
+    } else {
+        for guess in allGuesses {
+            print(guess, " ")
+        }
+        print()
+    }
+}
+
+//input ask for guess
+
+func getInput() -> String?{
+    var invalidGuess = true
+    print("What is your guess?", terminator: "")
+    return readLine()
+    while invalidGuess {
+        let guess = Character(readLine()!)   }
+    invalidGuess = false
+    if (guess == " " || guess == "\n"){
+        invalidGuess = true
+    } else {
+        for letter in allGuesses {
+            if guess == letter {
+                invalidGuess = true
+            }
+        }
+    }
+}
+
+
+//func to print body
+//func to print outline
+//func to terminate after 7 guesses
+
+
+
+
+//func play() {
+//    printRightGuesses()
+//    printGuessedLetters()
+//    getInput()
+//
+//
+//}
+
+
 
